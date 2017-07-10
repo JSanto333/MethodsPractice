bbbabb67cc6ecf4abcde86f015fa97002b9235ec
Joe Santoiemmo <JSanto333@gmail.com>
2017-07-10 11:02:20 -0400

    Added several methods

--- MethodsPractice/Program.cs
+++ MethodsPractice/Program.cs
@@ -19,21 +19,24 @@ namespace MethodsPractice
             //However, methods are frequently CALLED inside of another class or method.
 
 
-            Console.WriteLine("Hello. Please enter the first number to be added.");
-            int numberOne = int.Parse(Console.ReadLine());
-            Console.WriteLine("Please enter the second number to be added.");
-            int numberTwo = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Hello. Please enter the first number to be added.");
+            //int numberOne = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Please enter the second number to be added.");
+            //int numberTwo = int.Parse(Console.ReadLine());
 
-            int answer = Add(numberOne, numberTwo);
+            //int answer = Add(numberOne, numberTwo);
 
-            Console.WriteLine("The sum of your two numbers is " + answer);
+            //Console.WriteLine("The sum of your two numbers is " + answer);
 
+            //RobotWarning("Will Robinson");
+            //Useless();
 
+            string myBirthMonth = "September";
+            string myFriendBirthMonth = "December";
+            string myVehicle = Vehicle(myBirthMonth);
+            string myFriendVehicle = Vehicle(myFriendBirthMonth);
 
-
-
-
-
+            Console.WriteLine("My future vehicle is {0} and Jordan's future vehicle {1}.", myVehicle, myFriendVehicle);
         }
 
         //Method header
@@ -50,7 +53,34 @@ namespace MethodsPractice
             //and send that value back to where I call my method
         }
         //If you have a method that does not have a return value, you would use the keyword
-        //"void". When we have a "void" return type we DON'T need to use  
+        //"void". When we have a "void" return type we DON'T need to use the keyword "return"
+
+        public static void RobotWarning(string name)
+        {
+            Console.WriteLine("Danger, " + name + "!!");
+        }
+
+        public static void Useless()
+        {
+            Console.WriteLine("Blah blah blah");
+        }
+
+
+
+        public static string Vehicle(string birthMonth)
+        {
+            string vehicleFortune;
+            if (birthMonth.ToUpper() == "SEPTEMBER" || birthMonth.ToUpper() == "OCTOBER")
+            {
+                vehicleFortune = "Hoverboard";
+            }
+            else
+            {
+                vehicleFortune = "Web swinging";
+            }
+
+            return vehicleFortune;
+        }
 
 
     }
